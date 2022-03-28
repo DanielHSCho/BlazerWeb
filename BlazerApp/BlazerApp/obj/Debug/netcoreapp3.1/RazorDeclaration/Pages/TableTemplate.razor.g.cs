08c0d766d7ca9f1309185a305b2c7e5b0bb8d8de
daniel <hellena171791@gmail.com>
2022-03-28 15:00:09 +0900

    <파일 업데이트>

--- BlazerApp/BlazerApp/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/TableTemplate.razor.g.cs
+++ BlazerApp/BlazerApp/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/TableTemplate.razor.g.cs
@@ -75,35 +75,13 @@ using BlazerApp.Shared;
 #line default
 #line hidden
 #nullable disable
-#nullable restore
-#line 3 "C:\Users\helle\Projects\2_WebServer\BlazerApp\BlazerApp\BlazerApp\Pages\FetchData.razor"
-using BlazerApp.Data;
-
-#line default
-#line hidden
-#nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/fetchdata")]
-    public partial class FetchData : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class TableTemplate : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 39 "C:\Users\helle\Projects\2_WebServer\BlazerApp\BlazerApp\BlazerApp\Pages\FetchData.razor"
-       
-    private WeatherForecast[] forecasts;
-
-    protected override async Task OnInitializedAsync()
-    {
-        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
-    }
-
-#line default
-#line hidden
-#nullable disable
-        [global::Microsoft.AspNetCore.Components.InjectAttribute] private WeatherForecastService ForecastService { get; set; }
     }
 }
 #pragma warning restore 1591
