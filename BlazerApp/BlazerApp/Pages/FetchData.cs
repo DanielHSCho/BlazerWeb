fbe068e80f0363410a506513186f2494de9eedeb
daniel <hellena171791@gmail.com>
2022-03-28 14:54:06 +0900

    <blazer partial> razer 컴포넌트의 Code부분 c# 파일로 분리해보기
    - partial 쓰면 코드랑 UI부분 분리 가능
    - 굳이 분리할 필요 없어서 그냥 알고만 있을 것

---
+++ BlazerApp/BlazerApp/Pages/FetchData.cs
@@ -0,0 +1,18 @@
+﻿using BlazerApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazerApp.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
