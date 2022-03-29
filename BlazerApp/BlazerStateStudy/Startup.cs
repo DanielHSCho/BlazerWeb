f812ab5d2e1d5938af3a33b6284a4fb417fb349c
daniel <hellena171791@gmail.com>
2022-03-29 15:20:31 +0900

    <blazer State> Counter증가하고 페이지 바뀌었을 때 State 유지하기
    - 예전 예제에서 배운 Dependency Injection 사용
    - Singleton // 모든 유저가 동일한 state를 봐야할 때
    - Scoped // 유저별로 개별적인 state를 봐야할 때

--- BlazerApp/BlazerStateStudy/Startup.cs
+++ BlazerApp/BlazerStateStudy/Startup.cs
@@ -29,6 +29,8 @@ namespace BlazerStateStudy
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddScoped<CounterState>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
