e74addd33a92b17c2c09766fc06984b1e1c3a8b9
daniel <hellena171791@gmail.com>
2022-03-29 14:47:13 +0900

    <blazer Validation> 값 체크할 때 쓰는 Required 태그

--- BlazerApp/BlazerStudy/Data/WeatherForecast.cs
+++ BlazerApp/BlazerStudy/Data/WeatherForecast.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazerStudy.Data
 {
@@ -6,6 +7,7 @@ namespace BlazerStudy.Data
     {
         public DateTime Date { get; set; }
 
+        [Required(ErrorMessage = "Need TemperatureC!")]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
