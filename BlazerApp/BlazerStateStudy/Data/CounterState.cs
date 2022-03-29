b3331395e07e5b277877289494e06a2332bd80ad
daniel <hellena171791@gmail.com>
2022-03-29 15:32:57 +0900

    <blazer State 관리> 의존성 주입 사용 시 갱신 안되는 부분도 갱신되도록 개선

--- BlazerApp/BlazerStateStudy/Data/CounterState.cs
+++ BlazerApp/BlazerStateStudy/Data/CounterState.cs
@@ -7,6 +7,25 @@ namespace BlazerStateStudy.Data
 {
     public class CounterState
     {
-        public int Count { get; set; }
+        int _count = 0;
+
+        public Action OnStatChanged;
+
+        public int Count
+        {
+            get {
+                return _count;
+            }
+
+            set {
+                _count = value;
+                Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            OnStatChanged?.Invoke();
+        }
     }
 }
