3134a3db13396ddb0944f5f5b6243fce913134d3
daniel <hellena171791@gmail.com>
2022-03-29 14:48:56 +0900

    <blazer Validation> Range나 StringLength로 값의 범위도 지정할 수 있음

--- BlazerApp/BlazerStudy/Data/WeatherForecast.cs
+++ BlazerApp/BlazerStudy/Data/WeatherForecast.cs
@@ -8,8 +8,11 @@ namespace BlazerStudy.Data
         public DateTime Date { get; set; }
 
         [Required(ErrorMessage = "Need TemperatureC!")]
+        [Range(typeof(int), "-100", "100")]
         public int TemperatureC { get; set; }
 
+        [Required(ErrorMessage ="Need Summary!")]
+        [StringLength(10, MinimumLength =2, ErrorMessage ="2-10")]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
